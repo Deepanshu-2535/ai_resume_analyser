3d48dfa290a62ff1db3e441997b8cdd6c34d7bdb
dgmp755 <dgmp755@gmail.com>
2025-10-18 01:51:47 +0530

    login and signup page ui updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -43,43 +43,45 @@ const Home = ({setIsLoggedIn}) => {
         setVisible(v=>!v);
     }
   return (
-    <div className='min-h-screen min-w-screen flex items-center justify-center -z-1 bg-[url("./images/background.png")]'>
+    <div className='min-h-screen min-w-screen flex items-center justify-center -z-1 bg-[url("./images/login-new.png")] bg-cover'>
         <div className='min-h-screen min-w-[60%] items-center justify-center hidden md:flex'>
             <div className='px-10'>
-                <h1 className='text-7xl text-white font-sans text-center mb-3'><span className='font-bold text-blue-600'>Resu</span>Metrics</h1>
-                <p className='text-white/80 font-sans text-xl text-center'>AI-Powered Career Insights</p>
+                <h1 className='text-7xl text-black font-sans text-center mb-3'><span className='font-bold text-blue-600'>Resu</span>Metrics</h1>
+                <p className='text-black/80 font-sans text-xl text-center'>AI-Powered Career Insights</p>
             </div>
         </div>
-        <div className='min-h-screen min-w-screen md:min-w-[40%] bg-base-100 p-10 py-28 md:p-30'>
-            <div className='mb-20'>
-                <div className='flex items-center justify-center md:hidden p-0 m-0'>
-                    <h2 className='md:hidden mb-5 text-4xl text-center text-base-content font-sans w-80 m-0'>AI Powered Resume Analyser</h2>
-                </div>
-                <h2 className='text-4xl text-center md:text-left text-base-content font-sans underline'>Log In</h2>
-            </div>
-            <div className='m-4 md:mx-0'>
-                <label className='label mb-1'>User Id</label>
-                <br />
-                <input type="text" className='input focus:border-none mb-3 w-full md:w-xs' onChange={(e)=>setUserId(e.target.value)} />
-                <br />
-                <label className='label mb-1'>Password</label>
-                <br />
-                <div className='input focus:border-0 mb-10 w-full md:w-xs'>
-                    <input type={visible?"text":"password"} className='input focus:border-none focus:outline-none' onChange={(e)=>setPassword(e.target.value)}/>
-                    <div>
-                        <button className='btn btn-ghost btn-circle hover:bg-base-200/60 hover:border-0 hover:outline-0' onClick={toggleVisibility}>
-                            {visible?<EyeOff className='text-base-content/50 m-0'/>:<Eye className='text-base-content/50 m-0'/>}
-                        </button>
+        <div className='min-h-screen min-w-screen flex items-center justify-center md:min-w-[40%] p-5 py-28 md:p-0'>
+            <div className='md:px-20 px-5 md:py-30 py-20 pt-15 bg-white rounded-4xl shadow-2xl'>
+                <div className='mb-15'>
+                    <div className='flex items-center justify-center md:hidden p-0 m-0'>
+                        <h2 className='md:hidden mb-10 text-4xl text-center text-base-content font-sans w-80 m-0'><span className='font-bold text-blue-600'>Resu</span>Metrics</h2>
                     </div>
+                    <h2 className='text-4xl text-center md:text-left text-base-content font-[Montserrat] font-semibold'>Log In</h2>
                 </div>
-                <br />
-                <button className='btn btn-primary w-full md:w-xs mb-3' disabled={loggingIn} onClick={handleSubmit}>
-                    {loggingIn?"Logging In...":"Log In"}
-                </button>
-                <br />
-                <button className='btn btn-secondary w-full md:w-xs' onClick={()=>{navigate('/signup')}}>
-                    Sign Up
-                </button>
+                <div className='m-4 md:mx-0'>
+                    <label className='label mb-1 font-[montserrat] text-sm'>User Id</label>
+                    <br />
+                    <input type="text" className='input focus:border-none mb-3 w-full md:w-xs rounded-full' onChange={(e)=>setUserId(e.target.value)} />
+                    <br />
+                    <label className='label mb-1 font-[montserrat] text-sm'>Password</label>
+                    <br />
+                    <div className='input focus:border-0 mb-10 rounded-full w-full md:w-xs'>
+                        <input type={visible?"text":"password"} className='input focus:border-none focus:outline-none' onChange={(e)=>setPassword(e.target.value)}/>
+                        <div>
+                            <button className='btn btn-ghost btn-circle hover:bg-base-200/60 hover:border-0 hover:outline-0' onClick={toggleVisibility}>
+                                {visible?<EyeOff className='text-base-content/50 m-0'/>:<Eye className='text-base-content/50 m-0'/>}
+                            </button>
+                        </div>
+                    </div>
+                    <br />
+                    <button className='btn bg-blue-600 text-white w-full md:w-xs mb-3 rounded-full' disabled={loggingIn} onClick={handleSubmit}>
+                        {loggingIn?"Logging In...":"Log In"}
+                    </button>
+                    <br />
+                    <button className='btn bg-white rounded-full border-black w-full md:w-xs' onClick={()=>{navigate('/signup')}}>
+                        Sign Up
+                    </button>
+                </div>
             </div>
         </div>
     </div>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -58,44 +58,49 @@ const Signup = ({setIsLoggedIn}) => {
         setVisible(v=>!v);
     }
   return (
-    <div className='min-h-screen flex items-center justify-center -z-1 bg-[url("src/images/background.png")]'>
+    <div className='min-h-screen min-w-screen flex items-center justify-center bg-cover -z-1 bg-[url("src/images/login-new.png")]'>
             <div className='min-h-screen min-w-[60%] items-center justify-center hidden md:flex'>
                 <div className='px-10'>
-                    <h1 className='text-7xl text-white font-sans font-bold text-center mb-7'>Sign up</h1>
-                    <p className='text-white/80 font-sans font-light text-center'>Join thousands of professionals who are landing their dream jobs. Our AI resume analyzer provides instant, personalized feedback to help you stand out. Sign up today and get your free resume analysis!</p>
+                    <h1 className='text-7xl text-black font-sans text-center mb-3'><span className='font-bold text-blue-600'>Resu</span>Metrics</h1>
+                    <p className='text-black/80 font-sans text-xl text-center'>AI-Powered Career Insights</p>
                 </div>
             </div>
-            <div className='min-h-screen min-w-screen md:min-w-[40%] bg-base-100 p-10 py-28 md:p-30'>
-                <div className='mb-20'>
-                    <h2 className='text-4xl text-left text-base-content font-sans underline mx-4 md:mx-0'>Sign Up</h2>
-                </div>
-                <div className='mx-4 md:mx-0'>
-                    <label className='label mb-1'>User Id</label>
-                    <br />
-                    <input type="text" className='input focus:border-none mb-3 w-full md:w-xs' onChange={(e)=>setUserId(e.target.value)}/>
-                    <br />
-                    <label className='label mb-1'>Password</label>
-                    <br />
-                    <div className='input focus:border-0 mb-3 w-full md:w-xs'>
-                        <input type={visible?"text":"password"} className='input focus:border-none focus:outline-none' onChange={(e)=>setPassword(e.target.value)}/>
-                        <div>
-                            <button className='btn btn-ghost btn-circle hover:bg-base-200/60 hover:border-0 hover:outline-0' onClick={toggleVisibility}>
-                                {visible?<EyeOff className='text-base-content/50 m-0'/>:<Eye className='text-base-content/50 m-0'/>}
-                            </button>
+            <div className='min-h-screen min-w-screen flex items-center justify-center md:min-w-[40%] md:p-0'>
+                <div className='md:px-20 px-5 md:py-30 py-20 pt-15 bg-white rounded-4xl shadow-2xl'>
+                    <div className='mb-15'>
+                        <div className='flex items-center justify-center md:hidden p-0 m-0'>
+                            <h2 className='md:hidden mb-10 text-4xl text-center text-base-content font-sans w-80 m-0'><span className='font-bold text-blue-600'>Resu</span>Metrics</h2>
                         </div>
+                        <h2 className='text-4xl text-center md:text-left text-base-content font-[Montserrat] font-semibold mx-4 md:mx-0'>Sign Up</h2>
                     </div>
-                    <br />
-                    <label className='label mb-1'>Name</label>
-                    <br />
-                    <input type="text" className='input focus:border-none mb-3 w-full md:w-xs' onChange={(e)=>setName(e.target.value)}/>
-                    <br />
-                    <label className='label mb-1'>Email Id</label>
-                    <br />
-                    <input type="text" className='input focus:border-none mb-10 w-full md:w-xs' onChange={(e)=>setEmail(e.target.value)}/>
-                    <br />
-                    <button className='btn btn-primary w-full md:w-xs mb-3' disabled={signingUp} onClick={handleSubmit}>
-                        {signingUp?"Signing Up...":"Sign up"}
-                    </button>
+                    <div className='mx-4 md:mx-0'>
+                        <label className='label mb-1 font-[Montserrat] text-sm'>User Id</label>
+                        <br />
+                        <input type="text" className='input focus:border-none mb-3 w-full md:w-xs rounded-full' onChange={(e)=>setUserId(e.target.value)}/>
+                        <br />
+                        <label className='label mb-1 font-[Montserrat] text-sm'>Password</label>
+                        <br />
+                        <div className='input focus:border-0 mb-3 w-full md:w-xs rounded-full'>
+                            <input type={visible?"text":"password"} className='input focus:border-none focus:outline-none' onChange={(e)=>setPassword(e.target.value)}/>
+                            <div>
+                                <button className='btn btn-ghost btn-circle hover:bg-base-200/60 hover:border-0 hover:outline-0' onClick={toggleVisibility}>
+                                    {visible?<EyeOff className='text-base-content/50 m-0'/>:<Eye className='text-base-content/50 m-0'/>}
+                                </button>
+                            </div>
+                        </div>
+                        <br />
+                        <label className='label mb-1 font-[Montserrat] text-sm'>Name</label>
+                        <br />
+                        <input type="text" className='input focus:border-none mb-3 w-full md:w-xs rounded-full' onChange={(e)=>setName(e.target.value)}/>
+                        <br />
+                        <label className='label mb-1 font-[Montserrat] text-sm'>Email Id</label>
+                        <br />
+                        <input type="text" className='input focus:border-none mb-10 w-full md:w-xs rounded-full' onChange={(e)=>setEmail(e.target.value)}/>
+                        <br />
+                        <button className='btn bg-blue-600 rounded-full text-white w-full md:w-xs mb-3' disabled={signingUp} onClick={handleSubmit}>
+                            {signingUp?"Signing Up...":"Sign up"}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
