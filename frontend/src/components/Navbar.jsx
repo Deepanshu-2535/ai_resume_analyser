42bbf2362150047c7f91301263321e459cfd12fa
dgmp755 <dgmp755@gmail.com>
2025-10-18 23:31:45 +0530

    Navbar updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrainCircuit, CirclePlus } from 'lucide-react'
+import {CircleUserRound ,CirclePlus, ChevronDown } from 'lucide-react'
 import { Link, useNavigate, useParams } from 'react-router'
 
 const Navbar = () => {
@@ -9,22 +9,24 @@ const Navbar = () => {
         navigate(`/create/${userId}`);
     }
   return (
-    <div className="navbar bg-neutral shadow-sm text-neutral-content md:h-20 md:px-5">
+    <div className="navbar bg-white shadow-sm text-neutral-content h-25 md:h-25 md:px-5 flex justify-between">
         <div className="md:flex-1">
-           <Link to={`/dashboard/${userId}`}><span className="md:px-5 container flex items-center text-xl md:text-2xl font-semibold"><BrainCircuit className='mr-3'/>AI Resume Analyser</span></Link>
+           <Link to={`/dashboard/${userId}`}><span className="md:px-5 container flex items-center text-2xl text-black md:text-3xl font-semibold"><span className='font-bold text-blue-600'>Resu</span>Metrics</span></Link>
         </div>
-        <div className="flex-none">
-            <ul className="menu menu-horizontal px-1">
-            <li><button className='btn btn-base-100 btn-sm md:btn-md py-5 rounded-xl md:mr-3' onClick={handleclick}><CirclePlus/> Upload Resume</button></li>
-            <li>
-                <details>
-                <summary className='text-m'>Account</summary>
-                <ul className="bg-neutral rounded-t-none p-2">
+        <div className="flex items-center">
+            <button className='btn bg-blue-600 text-white btn-sm md:btn-md py-5 rounded-full md:mr-3' onClick={handleclick}><CirclePlus/> Upload Resume</button>
+            <div className="dropdown dropdown-end ml-2">
+                <div tabIndex={0} role="button" className="flex justify-around text-black/80 btn btn-ghost p-0"><CircleUserRound className='m-0'/><ChevronDown className='size-5 m-0'/></div>
+                <ul tabIndex="-1" className="dropdown-content menu bg-white rounded-box z-1 p-2 shadow-sm">
+                    <li className='text-black/80'><Link to={`/settings/${userId}`}>Settings</Link></li>
+                </ul>
+            </div>
+            {/* <details className='dropdown dropdown-end'>
+                <summary className='text-m text-black/80 btn btn-ghost p-2'><CircleUserRound/></summary>
+                <ul className="bg-white text-black p-2">
                     <li><Link to={`/settings/${userId}`}>Settings</Link></li>
                 </ul>
-                </details>
-            </li>
-            </ul>
+            </details> */}     
         </div>
     </div>
   )
